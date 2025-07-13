1e24ba740f662f312b0ce7be2deae867535fdf93
김혜영 <hyeyoung.k111@gmail.com>
2025-07-13 13:44:47 +0900

    토큰 초기화 코드 추가

--- tests/auth.practice.setup.ts
+++ tests/auth.practice.setup.ts
@@ -19,5 +19,26 @@ setup("Create customer auth", async ({ page, context }) => {
   await expect(page.getByRole("button", { name: "Jane Doe" })).toContainText(
     "Jane Doe"
   );
+
+  // local storage에 토큰 확인
+  // await page.waitForFunction(() => localStorage.getItem("auth-token") != null);
+  // let token = await page.evaluate(() => {
+  //   return localStorage.getItem("auth-token");
+  // });
+  // console.log("토큰 값:", token);
+
+  // 토큰 저장 대기
   await context.storageState({ path: customerAuthFile });
+
+  // 스토리지 초기화(토큰 유지시간 2분)
+  await page.evaluate(() => {
+    // evaulate : 즉시 실행, addInitScript : 이후 새 브라우저 실행 시 실행
+    localStorage.clear();
+  });
+
+  // local storage에 토큰 확인
+  // token = await page.evaluate(() => {
+  //   return localStorage.getItem("auth-token");
+  // });
+  // console.log("토큰 값:", token);
 });
