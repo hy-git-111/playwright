9b1049049a4777ae48c1955ea5b8e713127f5f24
김혜영 <hyeyoung.k111@gmail.com>
2025-07-13 17:06:42 +0900

    파일명 수정

--- tests/auth_practice.setup.ts
+++ tests/auth_practice.setup.ts
@@ -1,7 +1,8 @@
 import { test as setup, expect } from "@playwright/test";
 
 setup("Create customer auth", async ({ page, context }) => {
-  const email = "customer@practicesoftwaretesting.com";
+  // const email = "customer@practicesoftwaretesting.com";
+  const email = "customer2@practicesoftwaretesting.com";
   const password = "welcome01";
   const customerAuthFile = ".auth/customer.json";
 
@@ -16,29 +17,17 @@ setup("Create customer auth", async ({ page, context }) => {
   // 제출 버튼 클릭
   await page.getByTestId("login-submit").click();
 
-  await expect(page.getByRole("button", { name: "Jane Doe" })).toContainText(
-    "Jane Doe"
-  );
-
-  // local storage에 토큰 확인
-  // await page.waitForFunction(() => localStorage.getItem("auth-token") != null);
-  // let token = await page.evaluate(() => {
-  //   return localStorage.getItem("auth-token");
-  // });
-  // console.log("토큰 값:", token);
+  await expect(
+    page.getByRole("button", { name: /Jane Doe|Jack Howe/ })
+  ).toBeVisible();
 
   // 토큰 저장 대기
   await context.storageState({ path: customerAuthFile });
 
   // 스토리지 초기화(토큰 유지시간 2분)
-  await page.evaluate(() => {
+  await page.addInitScript(() => {
     // evaulate : 즉시 실행, addInitScript : 이후 새 브라우저 실행 시 실행
     localStorage.clear();
   });
 
-  // local storage에 토큰 확인
-  // token = await page.evaluate(() => {
-  //   return localStorage.getItem("auth-token");
-  // });
-  // console.log("토큰 값:", token);
 });
